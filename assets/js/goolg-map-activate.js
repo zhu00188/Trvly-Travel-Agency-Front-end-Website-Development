ba36830a4a29c624dbbf6e6f86a9618f911ed2d9
Jiaqi Zhu <zhu00188@algonquinlive.com>
2024-10-28 22:15:34 -0400

    Updated on Oct 28, 2024

---
+++ assets/js/goolg-map-activate.js
@@ -0,0 +1,42 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: new google.maps.LatLng(45.000000, -75.000000),
+        disableDefaultUI: true,
+        styles: [
+            {
+                elementType: 'labels',
+                stylers: [{
+                    visibility: 'on'
+                }]
+            },
+            {
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#E1E6EE'
+                }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#ededed'
+                }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#D8DDE5'
+                }]
+            },
+            {
+                featureType: "road",
+                elementType: "labels",
+                stylers: [
+                    { "visibility": "off" }
+                ]
+            }
+        ]
+    });
+}
